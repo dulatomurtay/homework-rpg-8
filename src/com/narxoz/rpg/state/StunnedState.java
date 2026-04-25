6e81dd55b92dd22411006b623ce119206ebe6df0
dulatomurtay <dulat.omurtai@narxoz.kz>
2026-04-25 14:35:05 +0500

    Add Stunned state

---
+++ src/com/narxoz/rpg/state/StunnedState.java
@@ -0,0 +1,25 @@
+package com.narxoz.rpg.state;
+
+import com.narxoz.rpg.combatant.Hero;
+
+public class StunnedState implements HeroState {
+
+    private int turns = 1;
+
+    public String getName() { return "Stunned"; }
+
+    public int modifyOutgoingDamage(int basePower) { return 0; }
+
+    public int modifyIncomingDamage(int rawDamage) { return rawDamage; }
+
+    public void onTurnStart(Hero hero) {}
+
+    public void onTurnEnd(Hero hero) {
+        turns--;
+        if (turns <= 0) {
+            hero.setState(new NormalState());
+        }
+    }
+
+    public boolean canAct() { return false; }
+}
